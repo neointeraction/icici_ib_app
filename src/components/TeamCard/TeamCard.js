efee376fdc5de987e8fe09697c996fd397eba45f
shameer237 <47691836+shameer237@users.noreply.github.com>
2021-12-09 01:53:47 +0530

    - added our team page and team  modal
    - added our team page and team modal

--- src/components/TeamCard/TeamCard.js
+++ src/components/TeamCard/TeamCard.js
@@ -1,31 +1,48 @@
-import React, { useState } from "react";
+import React from "react";
+
+import useModal from "../../hooks/useModal";
+import Modal from "../Modal";
 
 const TeamCard = ({ image, name, designation, summary }) => {
-  const [hover, setHover] = useState(false);
-
-  const handleCardHover = () => {
-    setHover(!hover);
-  };
+  const { toggle, visible } = useModal();
 
   return (
-    <div
-      className="team-card-container"
-      onMouseEnter={handleCardHover}
-      onMouseLeave={handleCardHover}
-    >
-      <div className="tc-upfront">
-        <div className="team-img">
-          <img src={image} alt="team-profile-pic" />
-        </div>
-        <div className={`team-info ${hover ? "tc-onhover" : "hide"}`}>
-          <h4 className="team-name">{name} </h4>
-          <h4 className="team-designation">{designation}</h4>
-          <div className={`team-summary ${hover ? "show" : null}`}>
-            <p>{summary}</p>
+    <>
+      <div className="team-card-container" onClick={toggle}>
+        <div className="tc-upfront">
+          <div className="team-img">
+            <img src={image} alt="team-profile-pic" />
+          </div>
+          <div className={`team-info `}>
+            <h4 className="team-name">{name} </h4>
+            <h4 className="team-designation">{designation}</h4>
           </div>
         </div>
       </div>
-    </div>
+
+      <Modal visible={visible} toggle={toggle}>
+        <div className="modal-content">
+          <div className="modal-profile">
+            <div className="row">
+              <div className="col-md-8">
+                <div className="team-img">
+                  <img src={image} alt="team-profile-pic" />
+                </div>
+              </div>
+            </div>
+            <div className={`team-info modal-team-info `}>
+              <h4 className="team-name text-left modal-team-name">{name} </h4>
+              <h4 className="team-designation text-left modal-team-designation">
+                {designation}
+              </h4>
+            </div>
+          </div>
+          <div className="summary-text">
+            <p className="modal-body-text"> {summary} </p>
+          </div>
+        </div>
+      </Modal>
+    </>
   );
 };
 
--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -5,10 +5,12 @@ import FadeInWhenVisible from "../../hooks/FadeInWhenVisible";
 import Title from "../../components/Title/Title";
 import TeamCard from "../../components/TeamCard";
 
-import IEBanner from "../../assets/images/ie-banner.jpg";
+import TeamBanner from "../../assets/images/team-banner.jpg";
 import Nimesh from "../../assets/images/team/nimesh.jpg";
 import Sankaran from "../../assets/images/team/sankaran.jpg";
 import Rahul from "../../assets/images/team/rahul.jpg";
+import Amit from "../../assets/images/team/amit.jpg";
+import Shamit from "../../assets/images/team/shamit.jpg";
 
 const data = [
   {
@@ -51,6 +53,24 @@ const data = [
     summary:
       "Rahul is the Chief Investment Officer for the AMC’s Fixed Income desk. In his role as CIO, Rahul is responsible for laying out the fixed income strategy for assets of over USD 20bn, the largest in the Indian Asset Management Industry. He also has direct fund management responsibilities for several funds. He also advises certain funds domiciled in Taiwan. Rahul has been associated with the AMC since July 2004. He served as a Senior Rates Trader and Head of Primary Dealership Business at Standard Chartered Bank for a three year period (2009 to 2012) before returning to the AMC in Sept 2012.Rahul is the Chief Investment Officer for the AMC’s Fixed Income desk. In his role as CIO, Rahul is responsible for laying out the fixed income strategy for assets of over USD 20bn, the largest in the Indian Asset Management Industry. He also has direct fund management responsibilities for several funds. He also advises certain funds domiciled in Taiwan. Rahul has been associated with the AMC since July 2004. He served as a Senior Rates Trader and Head of Primary Dealership Business at Standard Chartered Bank for a three year period (2009 to 2012) before returning to the AMC in Sept 2012. Rahul possesses an in-depth knowledge of credit profile of Indian entities. Under his leadership as a CIO, several funds managed by the AMC have gained recognition for consistently delivering superior risk-adjusted performance. Rahul has more than two decades of experience in Indian fixed income market. Prior to joining the AMC in July 2004, Rahul was a fund manager with the Fixed Income team of Franklin Templeton Asset Management in India. He has also been associated with leading debt broking outfits in India for several years. By qualification, Rahul holds a Bachelor’s Degree in Science and also Masters in Business Administration from University of Bhopal, India.",
   },
+  {
+    image: Amit,
+    name: "Mr. Amit Bhosale",
+    designation: "Head Risk Management",
+    summary: `Amit joined the AMC in 2014. In his role, Amit reports directly to the MD & CEO of the AMC. His team does not have any profit targets and is responsible for assessing risks emerging out of an investment strategy or introduction of new products/ processes.
+    Prior to joining the AMC, Amit worked as the Director, Financial Institutions Credit Risk at Bank of America (BoA). At BoA, he was an active member of the Risk Governance set up being involved in portfolio reviews, sensitivity analysis, new product approvals, managing regulatory/ statutory audits and credit review exams.    
+    Amit has over two decades of experience in managing a set of diverse credits across various industries including Banks, Policy Institutions, Broker Dealers, Insurance and Asset Managers. Over the years he has developed significant product expertise in handling syndicated loans, traded products (derivatives and fixed income issuances), trade finance, liquidity management products etc. In the past he has also been associated with leading organizations like ICICI Bank and Tata Motors.    
+    Amit has completed his Masters of Management Studies (MMS) from Jamnalal Bajaj Institute of Management Studies (JBIMS), Mumbai and holds a Bachelor of Engineering (B.E - Production) degree from Mumbai University.`,
+  },
+  {
+    image: Shamit,
+    name: "Mr. Shamit Chokshi",
+    designation: "Head – OFI, International Business",
+    summary: `Shamit joined the AMC in February 2016. He brings a rich and diverse industry experience across investments and research, products, asset raising, institutional client services and global strategic partnerships. His core competency lies in developing investment themes, managing equity and fixed income investments tailored for foreign investors and promoting the AMC’s investment capabilities to global institutional clients.
+    He has set up and managed several India and developed market funds globally across asset classes and showcased his expertise in asset raising and partnerships across markets.   
+    Prior to joining the AMC, Shamit was associated with Reliance Nippon Life Asset Management in Mumbai and Nippon Life Insurance group / Nissay in Tokyo where he led offshore investing and was instrumental in setting up the international funds franchise for the joint venture. Earlier he gained relevant equity research and funds experience with large financial firms in New York, Boston and Mumbai.    
+    Shamit pursued a Masters of Business Administration (Finance and Strategy) from the Carroll Graduate School of Management at Boston College, USA. He also holds a Bachelor of Commerce (Accounting) and Masters of Commerce (Banking & Finance) degree from the University of Mumbai, India. Shamit has completed the CFA program and is the holder of the right to use the CFA designation. He is an active member of the Mumbai CFA society.`,
+  },
 ];
 
 const Team = ({ hamOpen }) => {
@@ -62,7 +82,7 @@ const Team = ({ hamOpen }) => {
       className="page-container"
     >
       <div className="banner-static">
-        <img src={IEBanner} alt="IEBanner" />
+        <img src={TeamBanner} alt="TeamBanner" />
       </div>
       <div className="section-padding">
         <div className="container">
@@ -72,7 +92,7 @@ const Team = ({ hamOpen }) => {
           <FadeInWhenVisible>
             <div className="row">
               {data.map((item) => (
-                <div className="col-xl-6 col-lg-6">
+                <div className="col-xl-4 col-lg-4">
                   <TeamCard
                     image={item.image}
                     name={item.name}
